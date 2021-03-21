0edc6719ab5d07cbd78d180f6dceb1b94f7448ac
Igor.Kornilov <gaber32188@gmail.com>
2021-03-21 14:09:14 +0300

    block executions

---
+++ src/BlockChain.ts
@@ -0,0 +1,15 @@
+import { Block } from './Block';
+
+export class Blockchain {
+	private readonly chain: Block[] = [];
+	private get latestBlock(): Block {
+		return this.chain[this.chain.length - 1];
+	}
+	constructor() {
+		this.chain.push(new Block(0, '0', Date.now(), 'Genesis block'));
+	}
+	addBlock(data: string): void {
+		const block = new Block(this.latestBlock.index + 1, this.latestBlock.hash, Date.now(), data);
+		this.chain.push(block);
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Blockchain } from './BlockChain';
+
+console.log('Creating the blockchain with the genesis block...');
+const blockchain = new Blockchain();
+console.log('Mining block #1...');
+blockchain.addBlock('First block');
+console.log('Mining block #2...');
+blockchain.addBlock('Second block');
+console.log(JSON.stringify(blockchain, null, 2));
